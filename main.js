14b6f025574b8e5e90c8aac0d007b43c1da602b1
nhuytran122 <nhuytran12223@gmail.com>
2022-04-16 20:43:25 +0700

    upload source code

---
+++ main.js
@@ -0,0 +1,15 @@
+// confirm('Ban la sinh vien?');
+// console.log('Đây là web của tớ');
+// prompt('Bạn là sinh viên năm mấy nhỉ?');
+
+function myFunction() {
+	alert('Đăng ký tài khoản thành công! Bạn có thể đăng nhập rồi nhé!');
+}
+
+function btnCmt(){
+	alert('Cảm ơn lời góp ý của bạn!')
+}
+
+function btnRate(){
+	alert ('Cảm ơn đánh giá của bạn! Hãy tiếp tục trải nghiệm web nhé!')
+}
