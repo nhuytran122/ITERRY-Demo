62936237e9be96939cdcee652170e9d1d133723a
nhuytran122 <nhuytran12223@gmail.com>
2022-04-20 10:31:09 +0700

    calc

--- main.js
+++ main.js
@@ -13,3 +13,25 @@ function btnCmt(){
 function btnRate(){
 	alert ('Cảm ơn đánh giá của bạn! Hãy tiếp tục trải nghiệm web nhé!')
 }
+
+var input = document.querySelector('.input');
+var items = Array.from(document.querySelectorAll('.item'));
+items.forEach(function(btn){
+	btn.addEventListener('click', function(){
+		if(input.innerHTML=='0')
+			input.innerHTML='';
+		if(btn.innerHTML=='AC')
+			input.innerHTML='0';
+		else if(btn.innerHTML=='DEL'){
+			var arrtext=Array.from(input.innerHTML);
+			arrtext.splice(arrtext.length-1,1);
+			if(arrtext.length!=0)
+				input.innerHTML=arrtext.join('');
+			else input.innerHTML='0';
+		}
+		else if(btn.innerHTML=='='){
+			input.innerHTML=eval(input.innerHTML);
+		}
+		else input.innerHTML+=btn.innerHTML;
+	})
+})
