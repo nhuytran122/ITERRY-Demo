69b419e135e6fb003821d54f72676b4e68d0d8b2
nhuytran122 <nhuytran12223@gmail.com>
2022-05-28 20:23:23 +0700

    /

--- main.js
+++ main.js
@@ -12,24 +12,33 @@ function btnRate(){
 
 
 var input = document.querySelector('.input');
-var items = Array.from(document.querySelectorAll('.item'));
+//Tạo mới một mảng
+var items = document.querySelectorAll('.item');
 items.forEach(function(btn){
 	btn.addEventListener('click', function(){
-		if(input.innerHTML=='0')
-			input.innerHTML='';
-		if(btn.innerHTML=='AC')
-			input.innerHTML='0';
-		else if(btn.innerHTML=='DEL'){
-			var arrtext=Array.from(input.innerHTML);
-			arrtext.splice(arrtext.length-1,1);
-			if(arrtext.length!=0)
-				input.innerHTML=arrtext.join('');
-			else input.innerHTML='0';
+		//Để tránh trường hợp: 0123 --> 123
+		if(input.innerHTML == '0')
+			input.innerHTML = '';
+			
+		if(btn.innerHTML == 'AC')
+			input.innerHTML = '0';
+
+		else if(btn.innerHTML == 'DEL'){
+			var arrtext = Array.from(input.innerHTML);
+			arrtext.splice(arrtext.length-1, 1);
+			if(arrtext.length != 0)
+				//Nối các phần tử của mảng lại với nhau thành một chuỗi, 
+				//các phần tử được ngăn cách nhau bởi kí tự:
+				input.innerHTML = arrtext.join('');
+			else input.innerHTML = '0';
 		}
-		else if(btn.innerHTML=='='){
-			input.innerHTML=eval(input.innerHTML);
+
+		else if(btn.innerHTML == '='){
+			//Giúp tính toán các string có số và các phép toán
+			input.innerHTML = eval(input.innerHTML);
 		}
-		else input.innerHTML+=btn.innerHTML;
+
+		else input.innerHTML += btn.innerHTML;
 	})
 })
 
